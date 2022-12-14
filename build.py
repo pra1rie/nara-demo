5a92e2347056b9495da42bbdd242675a85f01539
Ren <73724137+pra1rie@users.noreply.github.com>
2022-12-14 03:26:17 +0000

    lmao

--- build.py
+++ build.py
@@ -4,7 +4,8 @@ import os
 name = 'a.out'
 libs = '-lSDL2main -lSDL2 -lSDL2_image -lSDL2_ttf'
 
-os.system(f'gcc nara/*.c sauce/*.c -I nara {libs} -o {name}')
+os.system(f'gcc sauce/*.c -I nara/include -Lnara -lnara {libs} -o {name}')
+# os.system(f'gcc nara/*.c sauce/*.c -I nara {libs} -o {name}')
 # os.system('rm -rf *.o')
 if os.path.exists(name):
     os.system(f'./{name}')
